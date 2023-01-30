a49a4e616c10955f37cb6d7b69ffab740319a0fb
Aguiar575 <aguiarus99arthurus@gmail.com>
2023-01-30 08:39:56 -0300

    adding swagger to loggingService api

--- LoggingService/LoggingService.Api/Program.cs
+++ LoggingService/LoggingService.Api/Program.cs
@@ -5,12 +5,21 @@ using LoggingService.Api.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 LogConfiguration.ConfigureLogging();
 builder.Host.UseSerilog();
 
 builder.Services.AddTransient<ILogService, LogService>();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapPost("/logInformation", async (LogRequest request, ILogService service) => {
     try
